bb52a80858ebbb6d32d80a7b964d717d44e12db4
Robson Pinheiro <robsso605@gmail.com>
2025-01-19 14:41:59 -0500

    add função para atualizar array amigos

--- app.js
+++ app.js
@@ -3,7 +3,6 @@
 //Crie um array para armazenar os nomes
 let amigos = [];
 
-
 function adicionarAmigo(){
     //Capturar valor da entrada.
     let adicionarALista = document.getElementById('amigo').value;
@@ -12,14 +11,33 @@ function adicionarAmigo(){
     if (adicionarALista == ''){
         alert('Por favor, insira um nome.')
     }else {
-        // Cria um novo elemento li
-        let itemLista = document.createElement('li');
-        itemLista.appendChild(document.createTextNode(adicionarALista));
+        //Se o valor nome digitado for valido adicione a lista de amigos
+        amigos.push(adicionarALista);
 
-        // Adiciona o elemento li à lista ul
-        document.getElementById('listaAmigos').appendChild(itemLista);
+        //Atualiza função na tela
+        atualizarAmigo()
 
         //Limpar campo de entrada
         document.getElementById('amigo').value = '';
+    }
+}
+
+function atualizarAmigo(){
+    //Selecinando lista onde os amigos serão exibidos
+    let ul = document.getElementById('listaAmigos');
+    
+    //Limpa conteúdo atual da lista
+    ul.innerHTML = ''; 
+
+    //Pecorrer Array e criar elementos li
+    for (let itens = 0; itens < amigos.length; itens++){
+        
+        //Criando elemento li
+        let li = document.createElement('li');
+        //inserindo o nome no li na posição o item
+        li.textContent = amigos[itens];
+        //inserindo o li na ul 
+        
+        ul.appendChild(li);
     }
 }
